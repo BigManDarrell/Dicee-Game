657ec3130c9a9a8d34e88c6b5243b90435cf6f70
Darrell-Rafa <102950390+Darrell-Rafa@users.noreply.github.com>
2022-05-16 16:51:55 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+    document.querySelector(".player-1").textContent += " (" + randomNumber1 + ")";
+    document.querySelector(".player-2").textContent += " (" + randomNumber2 + ")";
+
+    randomImg1 = "images/dice" + randomNumber1 + ".png";
+    randomImg2 = "images/dice" + randomNumber2 + ".png";
+
+    document.querySelector(".img1").setAttribute("src",randomImg1);
+    document.querySelector(".img2").setAttribute("src",randomImg2);
+
+    if(randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerHTML = "🤩 Player 1 Wins!";
+    }
+    else if(randomNumber2 > randomNumber1){
+        document.querySelector("h1").innerHTML = "Player 2 Wins! 🤩";
+    }
+    else{
+        document.querySelector("h1").innerHTML = "Draw... 🤯";
+    }
+
+
+
